8161ff6b439ac553442e699d62fe52947f41382b
Andry Widya Putra <andrywidyaputra@gmail.com>
2023-04-07 00:04:03 +0700

    fix: makes search result readonly

--- django_google_search/admin.py
+++ django_google_search/admin.py
@@ -22,3 +22,6 @@ class SearchResult(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
